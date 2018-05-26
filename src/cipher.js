adf4ca00e311d82b55eefae942dbed41ffe72ba1
Valentina <vale.gutierrezc@gmail.com>
2018-05-26 18:59:52 -0400

    cipher done with uppercase

--- src/cipher.js
+++ src/cipher.js
@@ -1,8 +1,25 @@
-window.cipher = {
-  encode: () => {
-    /* Acá va tu código */
-  },
-  decode: () => {
-    /* Acá va tu código */
-  }
+// window.cipher = {
+//   encode: () => {
+//      Acá va tu código 
+//   },
+//   decode: () => {
+//      Acá va tu código 
+//   }
+// }
+
+function cipherFunction() {
+	var cipheredMessage = document.getElementById("message")
+	var messageTextArea = document.getElementById("cipherDecipherText").value;
+	var ciphered = "";
+	for (var index = 0; index < messageTextArea.length; index++) {
+		var asciiCode = messageTextArea.toUpperCase().charCodeAt(index);
+		if (asciiCode == 32) {
+			ciphered += String.fromCharCode(asciiCode);
+			continue;
+		}
+		var toLetters = String.fromCharCode((asciiCode  - 65 + 4) % 26 + 65);
+		ciphered += toLetters;
+	}
+	cipheredMessage.innerHTML = ciphered;
+	console.log(ciphered);
 }
