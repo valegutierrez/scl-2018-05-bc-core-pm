23cec417e81f03e3db83d9e8c9cd3bf59fe6d1c0
Valentina <vale.gutierrezc@gmail.com>
2018-05-26 21:24:37 -0400

    changing cipher encapsulation

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-/* Acá va tu código */
+const resultMessage = document.getElementById("message");
+// avisa dónde irá el mensaje una vez realizada la función más conveniente
+function cipherFunction() {
+	const offset = document.getElementById("number").value;
+	// saca el valor de la caja de desplazamiento y la vuelve variable
+	const string = document.getElementById("cipherDecipherText").value;
+	// saca el valor de la caja de texto y la vuelve variable
+	if (offset=="") {
+		alert("No se ha ingresado la cantidad de desplazamientos.");
+	}
+	else if (offset==0) {
+		alert("El número de desplazamientos no puede ser igual a 0.");
+	}
+	else {
+		resultMessage.innerHTML = cipherEncode(offset,string);
+	}
+}
